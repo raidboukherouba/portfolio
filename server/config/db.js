b3bd41747e40ab2d7d9617710c2db81e9e1bcb04
Boukherouba <101669644+Boukherouba@users.noreply.github.com>
2025-06-18 19:52:49 +0100

    Initial commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 async function connectDB() {
-  await mongoose.connect('mongodb+srv://dbPortfolio:dbPortfolio@raidboukherouba.e828e.mongodb.net/?retryWrites=true&w=majority&appName=raidboukherouba');
+  await mongoose.connect(process.env.MONGODB_URI);
   //mongodb://mongo:27017/portfolioDB
+  //mongodb+srv://dbPortfolio:dbPortfolio@raidboukherouba.e828e.mongodb.net/?retryWrites=true&w=majority&appName=raidboukherouba
   console.log('✅ Connected to MongoDB');
 }
 
